ae35f0dc3adfc4880a8e3de5fafeb763f7805e86
Jkasnese <guilhermelw@gmail.com>
2018-07-10 01:28:48 -0300

    Fixing merge to lcd controller

--- sources/lcd_controller.v
+++ sources/lcd_controller.v
@@ -1,6 +1,5 @@
 module lcd_controller(
 
-<<<<<<< HEAD
 input Clock, 
 input Reset,
 input [1:0] user_opt,
@@ -11,28 +10,6 @@ output [7:0] LCD_DATA,
 output idle
 );
 
-=======
-	// Entradas
-	 input Clock,  // 50  MHz
-	 input Reset,
-	 input [1:0] Modo_OP,
-
-	// Saídas
-
-	 output LCD_EN,
-	 output LCD_RS,
-	 output LCD_RW,
-	 output [7:0] LCD_DADOS,
-	 output idle,
-	 output [3:0] state,
-	 output flag_rst_out
-	);
-	
-	assign flag_rst_out = flag_rst;
-	assign state = reg_state;
-	
-	// Estadoss
->>>>>>> 886aeeb2d64fd09aa63e147fa6c2d49d3fa250be
 
     parameter   [3:0]   INIT = 4'b0000,
                         CONFIG = 4'b0001,
@@ -55,7 +32,6 @@ output idle
               t_250ns = 21'd13,
               t_1ms = 21'd50_000;
 
-<<<<<<< HEAD
 
 reg reg_en = 1'b0;
 reg reg_idle = 1'b0;
@@ -91,45 +67,6 @@ always @(posedge Clock) begin
 
 		if(counter >= t_250ns) begin
 			flag_250ns <= 1'b1;
-=======
-   parameter [7:0] letra_A = 8'b01000001; // Testar escrita 
-
-		
-   reg [3:0] next = 4'b0000, reg_state = 4'b0000;
-   reg reg_en = 1'b0, reg_rs = 1'b0; 
-	reg reg_rw = 1'b0;
-   reg [7:0] reg_db; // = 8'b11111111;
-   reg [5:0] time_counter = 6'd0;
-   reg num_of_lines = 1'b1, cursor_direction = 1'b1;
-	reg [15:0] clk_conversor;
-	reg reg_idle;
-
-	// Flags for delays
-	reg flag_40ms, flag_20ms, flag_10ms, flag_4ms, flag_1ms, flag_rst = 1'b0;
-		
-	 
-    // assign num_of_lines = Modo_OP[1];
-    // assign cursor_direction = Modo_OP[0];
-
-	// assign Colunas[0] = 1'b0;
-	// assign Colunas[4:1] = 4'b1111;
-    assign LCD_EN = reg_en;
-    assign LCD_RS = reg_rs;
-    assign LCD_RW = reg_rw;
-    assign LCD_DADOS = reg_db;
-
-    assign idle = reg_idle;
-
-	// Flags counter
-	always @ (posedge Clock or posedge Reset or posedge flag_rst) begin
-		if (Reset == 1'b1) begin
-			time_counter <= 6'd0;
-			flag_1ms <= 1'b0;
-			flag_4ms <= 1'b0;
-			flag_10ms <= 1'b0;
-			flag_20ms <= 1'b0;
-			flag_40ms <= 1'b0;
->>>>>>> 886aeeb2d64fd09aa63e147fa6c2d49d3fa250be
 		end
 		else begin
 			flag_250ns <= 1'b0;
@@ -161,7 +98,6 @@ always @(posedge Clock) begin
 	    end
 	end
 
-<<<<<<< HEAD
 	
 	always @ (posedge Clock) begin
 		
@@ -193,92 +129,6 @@ always @(posedge Clock) begin
 							end
 							2'b01: begin
 								if(prev_state == 2'b00) begin
-=======
-	// State machine clock
-    always @ (posedge Clock or posedge Reset) begin
-        if (Reset == 1'b1) begin
-				reg_state <= INIT;
-		  end
-		  else begin
-            reg_state <= next;
-        end
-    end
-
-	 
-    always @ (*) begin
-    	reg_en = 1'b0;
-    	flag_rst = 1'b0;
-	        case (reg_state)
-            INIT: begin
-            		reg_idle = 1'b0;
-					 if (flag_40ms == 1'b1) begin 
-							  reg_rs = 1'b0;
-							  reg_db = 8'b00110000;
-							  reg_en = 1'b1;
-							  next = CONFIG;
-							  flag_rst = 1'b1;
-						 end
-					 else begin   
-							  next = INIT; 
-						 end
-				  end
-
-            CONFIG:  begin
-                        if(flag_4ms == 1'b1) begin
-                                reg_rs = 1'b0;
-                                reg_db = 8'b00110000;
-							  	reg_en = 1'b1;
-  							    next = CONFIG_2;
-								 flag_rst = 1'b1;
-                            end
-                        else begin
-                            next = CONFIG;
-                        end
-                    end
-
-            CONFIG_2: begin
-								if (flag_1ms == 1'b1) begin
-										reg_rs = 1'b0;
-										reg_db = 8'b00110000;
-										reg_en = 1'b1;
-								next = FUNCTION_SET;
-								flag_rst = 1'b1;
-										next = CONFIG_2;
-								end
-                      	else begin
-                      		next = CONFIG_2;
-                      	end
-				end
-            FUNCTION_SET: begin               // Tem coisas ajustáveis
-            				if (flag_1ms == 1'b1) begin
-		                        reg_rs = 1'b0;
-		                        reg_db = 8'b0111001; // FUNCTION_SET {3'b0011, num_of_lines, 4'b001};
-		                        reg_en = 1'b1;
-								next = OSC_FREQ;
-								flag_rst = 1'b1;
-		                    end
-		                    else begin
-		                    	next = FUNCTION_SET;
-		                    end
-				end
-            OSC_FREQ: begin
-            			if (flag_1ms == 1'b1) begin
-	                        reg_rs = 1'b0;
-	                        reg_db = 8'b00010100; // OSC FREQ 8'b00001110;
-	                        reg_en = 1'b1;
-							next = POWER_CONTROL;
-							flag_rst = 1'b1;
-	                    end
-	                    else begin
-	                    	next = OSC_FREQ;
-	                    end
-				end
-            POWER_CONTROL: begin //power contrl
-            			if (flag_1ms == 1'b1) begin
-	                        reg_rs = 1'b0;
-	                        reg_db =  8'b01010110; // 8'b0111000; // POWR
-	                        reg_en = 1'b1;
->>>>>>> 886aeeb2d64fd09aa63e147fa6c2d49d3fa250be
 									flag_rst = 1'b1;
 								end
 								else begin
